3a67924d4b6e2cef016c297c9c5f24c68cf339e8
zgembo14 <152442631+zgembo14@users.noreply.github.com>
2023-11-30 01:07:48 +0100

    Update midjourney-api.js

--- midjourney-api.js
+++ midjourney-api.js
@@ -6,6 +6,8 @@ const data = {
  
 // we wrap it in a main function here so we can use async/await inside of it.
 async function main() {
+ let promptResponseData;
+    // generate the image
     try {
         const response = await fetch('https://demo.imagineapi.dev/items/images/', {
             method: 'POST',
@@ -15,6 +17,9 @@ async function main() {
             },
             body: JSON.stringify(data)
         });
+     
+        promptResponseData = await response.json();
+        console.log(promptResponseData);
  
         const responseData = await response.json();
         console.log(responseData);
@@ -22,6 +27,34 @@ async function main() {
         console.error('Error generating image:', error);
         throw error;
     }
+
+    / check if the image has finished generating
+    // let's repeat this code every 5000 milliseconds (5 seconds, set at the bottom)
+    const intervalId = setInterval(async function () {
+        try {
+          console.log('Checking image details');
+          const response = await fetch(`https://demo.imagineapi.dev/items/images/${promptResponseData.data.id}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer u667iLDO2Xfu0-qpv_nu82EeeDtYGlsf', // <<<< TODO: remember to change this
+                'Content-Type': 'application/json'
+            }
+          })
+ 
+          const responseData = await response.json();
+          if (responseData.data.status === 'completed' || responseData.data.status === 'failed') {
+            // stop repeating
+            clearInterval(intervalId);
+            console.log('Completed image detials', responseData.data);
+          } else {
+            console.log("Image is not finished generation. Status: ", responseData.data.status)
+          }
+        } catch (error) {
+          console.error('Error getting updates', error);
+          throw error;
+        }
+    }, 5000 /* every 5 seconds */);
+    // TODO: add a check to ensure this does not run indefinitely
 }
  
 main();
